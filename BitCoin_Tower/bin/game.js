d36f982febc98f9d19ace5cec8404c1176334ebe
Dorien Viliamu <79825132+straightdangerous@users.noreply.github.com>
2021-03-04 07:16:48 +1300

    Add files via upload

---
+++ BitCoin_Tower/bin/game.js
@@ -0,0 +1,39 @@
+
+const _scene = new Entity('_scene')
+engine.addEntity(_scene)
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+_scene.addComponentOrReplace(transform)
+
+const entity = new Entity('entity')
+engine.addEntity(entity)
+entity.setParent(_scene)
+const gltfShape = new GLTFShape("models/FloorBaseGrass_01/FloorBaseGrass_01.glb")
+gltfShape.withCollisions = true
+gltfShape.isPointerBlocker = true
+gltfShape.visible = true
+entity.addComponentOrReplace(gltfShape)
+const transform2 = new Transform({
+  position: new Vector3(8, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity.addComponentOrReplace(transform2)
+
+const finalBitCoinTower = new Entity('finalBitCoinTower')
+engine.addEntity(finalBitCoinTower)
+finalBitCoinTower.setParent(_scene)
+const transform3 = new Transform({
+  position: new Vector3(8, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+finalBitCoinTower.addComponentOrReplace(transform3)
+const gltfShape2 = new GLTFShape("models/210303 - hexagon watch tower - test 6.glb")
+gltfShape2.withCollisions = true
+gltfShape2.isPointerBlocker = true
+gltfShape2.visible = true
+finalBitCoinTower.addComponentOrReplace(gltfShape2)
